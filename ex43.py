cc52b3e7e5b03bcc33036d8373bac3567d2d93b9
Lawrence Lee <theinversesquarelaw@gmail.com>
2014-06-30 23:03:44 -0700

    Implemented Map class.

--- ex43.py
+++ ex43.py
@@ -176,3 +176,26 @@ class EscapePod(Scene):
             print "time. You won!"
             
             return 'finished'
+
+class Map(object):
+    
+    scenes = {
+        'central_corridor': CentralCorridor(),
+        'laser_weapon_armory': LaserWeaponArmory(),
+        'the_bridge': TheBridge(),
+        'escape_pod': EscapePod(),
+        'death': Death()
+    }
+
+    def __init__(self, start_scene):
+        self.start_scene = start_scene
+
+    def next_scene(self, scene_name):
+        return Map.scenes.get(scene_name)
+
+    def opening_scene(self):
+        return self.next_scene(self.start_scene)
+
+a_map = Map('central_corridor')
+a_game = Engine(a_map)
+a_game.play()
